77c769e1ad21f78159e1ea011e6dc86335095dc7
sellasouls666 <pgbc303@gmail.com>
2026-03-05 13:05:49 +0300

    закончил карточку

--- DemoForm/ProductCard.Designer.cs
+++ DemoForm/ProductCard.Designer.cs
@@ -33,18 +33,19 @@
             this.nameLabel = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.descriptionTextBox = new System.Windows.Forms.RichTextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.manufacturerTextBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.supplierTextBox = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.priceLabel = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.unitLabel = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.countNameLabel = new System.Windows.Forms.Label();
             this.countLabel = new System.Windows.Forms.Label();
             this.discountLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.manufacturerLabel = new System.Windows.Forms.Label();
+            this.supplierLabel = new System.Windows.Forms.Label();
+            this.newPriceLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +53,7 @@
             // 
             this.picBox.Location = new System.Drawing.Point(13, 14);
             this.picBox.Name = "picBox";
-            this.picBox.Size = new System.Drawing.Size(343, 300);
+            this.picBox.Size = new System.Drawing.Size(343, 241);
             this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picBox.TabIndex = 0;
             this.picBox.TabStop = false;
@@ -97,65 +98,31 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Описание товара:";
             // 
-            // descriptionTextBox
-            // 
-            this.descriptionTextBox.BackColor = System.Drawing.Color.Chartreuse;
-            this.descriptionTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.descriptionTextBox.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.descriptionTextBox.Location = new System.Drawing.Point(545, 43);
-            this.descriptionTextBox.Name = "descriptionTextBox";
-            this.descriptionTextBox.ReadOnly = true;
-            this.descriptionTextBox.Size = new System.Drawing.Size(281, 83);
-            this.descriptionTextBox.TabIndex = 5;
-            this.descriptionTextBox.Text = "";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(362, 140);
+            this.label3.Location = new System.Drawing.Point(362, 81);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(116, 19);
             this.label3.TabIndex = 6;
             this.label3.Text = "Производитель:";
             // 
-            // manufacturerTextBox
-            // 
-            this.manufacturerTextBox.BackColor = System.Drawing.Color.Chartreuse;
-            this.manufacturerTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.manufacturerTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.manufacturerTextBox.Location = new System.Drawing.Point(545, 137);
-            this.manufacturerTextBox.Name = "manufacturerTextBox";
-            this.manufacturerTextBox.ReadOnly = true;
-            this.manufacturerTextBox.Size = new System.Drawing.Size(281, 19);
-            this.manufacturerTextBox.TabIndex = 7;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(362, 183);
+            this.label4.Location = new System.Drawing.Point(362, 124);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 19);
             this.label4.TabIndex = 8;
             this.label4.Text = "Поставщик:";
             // 
-            // supplierTextBox
-            // 
-            this.supplierTextBox.BackColor = System.Drawing.Color.Chartreuse;
-            this.supplierTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.supplierTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.supplierTextBox.Location = new System.Drawing.Point(545, 180);
-            this.supplierTextBox.Name = "supplierTextBox";
-            this.supplierTextBox.ReadOnly = true;
-            this.supplierTextBox.Size = new System.Drawing.Size(281, 19);
-            this.supplierTextBox.TabIndex = 9;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(362, 226);
+            this.label5.Location = new System.Drawing.Point(362, 167);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(46, 19);
             this.label5.TabIndex = 10;
@@ -165,7 +132,7 @@
             // 
             this.priceLabel.AutoSize = true;
             this.priceLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.priceLabel.Location = new System.Drawing.Point(541, 226);
+            this.priceLabel.Location = new System.Drawing.Point(541, 167);
             this.priceLabel.Name = "priceLabel";
             this.priceLabel.Size = new System.Drawing.Size(45, 19);
             this.priceLabel.TabIndex = 11;
@@ -175,7 +142,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(362, 259);
+            this.label6.Location = new System.Drawing.Point(362, 200);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(148, 19);
             this.label6.TabIndex = 12;
@@ -185,27 +152,27 @@
             // 
             this.unitLabel.AutoSize = true;
             this.unitLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.unitLabel.Location = new System.Drawing.Point(541, 259);
+            this.unitLabel.Location = new System.Drawing.Point(541, 200);
             this.unitLabel.Name = "unitLabel";
             this.unitLabel.Size = new System.Drawing.Size(45, 19);
             this.unitLabel.TabIndex = 13;
             this.unitLabel.Text = "label7";
             // 
-            // label7
+            // countNameLabel
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(362, 295);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(160, 19);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Количество на складе:";
+            this.countNameLabel.AutoSize = true;
+            this.countNameLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countNameLabel.Location = new System.Drawing.Point(362, 236);
+            this.countNameLabel.Name = "countNameLabel";
+            this.countNameLabel.Size = new System.Drawing.Size(160, 19);
+            this.countNameLabel.TabIndex = 14;
+            this.countNameLabel.Text = "Количество на складе:";
             // 
             // countLabel
             // 
             this.countLabel.AutoSize = true;
             this.countLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.countLabel.Location = new System.Drawing.Point(541, 295);
+            this.countLabel.Location = new System.Drawing.Point(541, 236);
             this.countLabel.Name = "countLabel";
             this.countLabel.Size = new System.Drawing.Size(45, 19);
             this.countLabel.TabIndex = 15;
@@ -215,36 +182,76 @@
             // 
             this.discountLabel.AutoSize = true;
             this.discountLabel.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.discountLabel.Location = new System.Drawing.Point(878, 151);
+            this.discountLabel.Location = new System.Drawing.Point(853, 112);
             this.discountLabel.Name = "discountLabel";
             this.discountLabel.Size = new System.Drawing.Size(82, 31);
             this.discountLabel.TabIndex = 16;
             this.discountLabel.Text = "label8";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descriptionLabel.Location = new System.Drawing.Point(542, 46);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(38, 15);
+            this.descriptionLabel.TabIndex = 17;
+            this.descriptionLabel.Text = "label8";
+            // 
+            // manufacturerLabel
+            // 
+            this.manufacturerLabel.AutoSize = true;
+            this.manufacturerLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.manufacturerLabel.Location = new System.Drawing.Point(541, 81);
+            this.manufacturerLabel.Name = "manufacturerLabel";
+            this.manufacturerLabel.Size = new System.Drawing.Size(45, 19);
+            this.manufacturerLabel.TabIndex = 18;
+            this.manufacturerLabel.Text = "label8";
+            // 
+            // supplierLabel
+            // 
+            this.supplierLabel.AutoSize = true;
+            this.supplierLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.supplierLabel.Location = new System.Drawing.Point(541, 124);
+            this.supplierLabel.Name = "supplierLabel";
+            this.supplierLabel.Size = new System.Drawing.Size(45, 19);
+            this.supplierLabel.TabIndex = 19;
+            this.supplierLabel.Text = "label8";
+            // 
+            // newPriceLabel
+            // 
+            this.newPriceLabel.AutoSize = true;
+            this.newPriceLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.newPriceLabel.Location = new System.Drawing.Point(597, 167);
+            this.newPriceLabel.Name = "newPriceLabel";
+            this.newPriceLabel.Size = new System.Drawing.Size(0, 19);
+            this.newPriceLabel.TabIndex = 20;
             // 
             // ProductCard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Chartreuse;
+            this.Controls.Add(this.newPriceLabel);
+            this.Controls.Add(this.supplierLabel);
+            this.Controls.Add(this.manufacturerLabel);
+            this.Controls.Add(this.descriptionLabel);
             this.Controls.Add(this.discountLabel);
             this.Controls.Add(this.countLabel);
-            this.Controls.Add(this.label7);
+            this.Controls.Add(this.countNameLabel);
             this.Controls.Add(this.unitLabel);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.priceLabel);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.supplierTextBox);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.manufacturerTextBox);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.descriptionTextBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.categoryLabel);
             this.Controls.Add(this.picBox);
             this.Name = "ProductCard";
-            this.Size = new System.Drawing.Size(1000, 329);
+            this.Size = new System.Drawing.Size(977, 270);
             ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -258,17 +265,18 @@
         private System.Windows.Forms.Label nameLabel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox descriptionTextBox;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox manufacturerTextBox;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox supplierTextBox;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label priceLabel;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label unitLabel;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label countNameLabel;
         private System.Windows.Forms.Label countLabel;
         private System.Windows.Forms.Label discountLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Label manufacturerLabel;
+        private System.Windows.Forms.Label supplierLabel;
+        private System.Windows.Forms.Label newPriceLabel;
     }
 }
--- DemoForm/ProductCard.cs
+++ DemoForm/ProductCard.cs
@@ -26,9 +26,9 @@ namespace DemoForm
 
             categoryLabel.Text = product.category_;
             nameLabel.Text = product.name_;
-            descriptionTextBox.Text = product.description_;
-            manufacturerTextBox.Text = product.manufacturer_;
-            supplierTextBox.Text = product.supplier_;
+            descriptionLabel.Text = product.description_;
+            manufacturerLabel.Text = product.manufacturer_;
+            supplierLabel.Text = product.supplier_;
             priceLabel.Text = product.price_.ToString();
             unitLabel.Text = product.unit_;
             countLabel.Text = product.count_.ToString();
@@ -40,6 +40,41 @@ namespace DemoForm
             else
             {
                 picBox.Load("picture.png");
+            }
+
+            if (product.discount_ > 15)
+            {
+                this.BackColor = Color.FromName("#2E8B57");
+            }
+            else
+            {
+                this.BackColor = Color.FromName("Chartreuse");
+            }
+
+            if (product.discount_ != 0)
+            {
+                priceLabel.Font = new Font(priceLabel.Font, FontStyle.Strikeout);
+                priceLabel.ForeColor = Color.FromName("Red");
+
+                newPriceLabel.Text = (product.price_ - (product.price_ / 100 * product.discount_)).ToString();
+            }
+            else
+            {
+                priceLabel.Font = new Font(priceLabel.Font, FontStyle.Regular);
+                priceLabel.ForeColor = Color.FromName("ControlText");
+
+                newPriceLabel.Text = "";
+            }
+
+            if (product.count_ == 0)
+            {
+                countLabel.ForeColor = Color.FromName("DeepSkyBlue");
+                countNameLabel.ForeColor = Color.FromName("DeepSkyBlue");
+            }
+            else
+            {
+                countLabel.ForeColor = Color.FromName("ControlText");
+                countNameLabel.ForeColor = Color.FromName("ControlText");
             }
         }
     }
