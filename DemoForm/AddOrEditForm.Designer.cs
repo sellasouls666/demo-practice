0ab9c7529129e2cb09057f4c35f7b1a6b518c631
sellasouls666 <pgbc303@gmail.com>
2026-03-07 09:06:41 +0300

    сделал внешний вид AddOrEditForm

--- DemoForm/AddOrEditForm.Designer.cs
+++ DemoForm/AddOrEditForm.Designer.cs
@@ -28,12 +28,330 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.articulTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.unitTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.priceNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.supplierTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.manufacturerTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.discountNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.countNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label10 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.RichTextBox();
+            this.addPicButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discountNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(278, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Артикул:";
+            // 
+            // articulTextBox
+            // 
+            this.articulTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.articulTextBox.Location = new System.Drawing.Point(463, 13);
+            this.articulTextBox.Name = "articulTextBox";
+            this.articulTextBox.Size = new System.Drawing.Size(131, 26);
+            this.articulTextBox.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 16);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(243, 212);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(278, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Название:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameTextBox.Location = new System.Drawing.Point(463, 56);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(131, 26);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(278, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Единица измерения:";
+            // 
+            // unitTextBox
+            // 
+            this.unitTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.unitTextBox.Location = new System.Drawing.Point(463, 98);
+            this.unitTextBox.Name = "unitTextBox";
+            this.unitTextBox.Size = new System.Drawing.Size(131, 26);
+            this.unitTextBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(278, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Цена:";
+            // 
+            // priceNumeric
+            // 
+            this.priceNumeric.DecimalPlaces = 2;
+            this.priceNumeric.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.priceNumeric.Location = new System.Drawing.Point(463, 144);
+            this.priceNumeric.Name = "priceNumeric";
+            this.priceNumeric.Size = new System.Drawing.Size(131, 26);
+            this.priceNumeric.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(278, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Поставщик:";
+            // 
+            // supplierTextBox
+            // 
+            this.supplierTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.supplierTextBox.Location = new System.Drawing.Point(463, 182);
+            this.supplierTextBox.Name = "supplierTextBox";
+            this.supplierTextBox.Size = new System.Drawing.Size(131, 26);
+            this.supplierTextBox.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(278, 229);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(116, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Производитель:";
+            // 
+            // manufacturerTextBox
+            // 
+            this.manufacturerTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.manufacturerTextBox.Location = new System.Drawing.Point(463, 226);
+            this.manufacturerTextBox.Name = "manufacturerTextBox";
+            this.manufacturerTextBox.Size = new System.Drawing.Size(131, 26);
+            this.manufacturerTextBox.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(631, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 19);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Категория:";
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.categoryComboBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Items.AddRange(new object[] {
+            "Мужская обувь",
+            "Женская обувь"});
+            this.categoryComboBox.Location = new System.Drawing.Point(816, 13);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(131, 27);
+            this.categoryComboBox.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(631, 59);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 19);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Скидка (в %):";
+            // 
+            // discountNumeric
+            // 
+            this.discountNumeric.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.discountNumeric.Location = new System.Drawing.Point(816, 57);
+            this.discountNumeric.Name = "discountNumeric";
+            this.discountNumeric.Size = new System.Drawing.Size(131, 26);
+            this.discountNumeric.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(631, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(160, 19);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Количество на складе:";
+            // 
+            // countNumeric
+            // 
+            this.countNumeric.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countNumeric.Location = new System.Drawing.Point(816, 102);
+            this.countNumeric.Name = "countNumeric";
+            this.countNumeric.Size = new System.Drawing.Size(131, 26);
+            this.countNumeric.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(631, 147);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 19);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Описание:";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.descriptionTextBox.Location = new System.Drawing.Point(816, 148);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(131, 96);
+            this.descriptionTextBox.TabIndex = 20;
+            this.descriptionTextBox.Text = "";
+            // 
+            // addPicButton
+            // 
+            this.addPicButton.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.addPicButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addPicButton.Location = new System.Drawing.Point(12, 239);
+            this.addPicButton.Name = "addPicButton";
+            this.addPicButton.Size = new System.Drawing.Size(243, 35);
+            this.addPicButton.TabIndex = 21;
+            this.addPicButton.Text = "Загрузить изображение";
+            this.addPicButton.UseVisualStyleBackColor = false;
+            // 
+            // okButton
+            // 
+            this.okButton.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.okButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.okButton.Location = new System.Drawing.Point(835, 296);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(112, 33);
+            this.okButton.TabIndex = 22;
+            this.okButton.Text = "ОК";
+            this.okButton.UseVisualStyleBackColor = false;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.cancelButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(39, 296);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(112, 33);
+            this.cancelButton.TabIndex = 23;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            // 
+            // AddOrEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(976, 341);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.addPicButton);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.countNumeric);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.discountNumeric);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.manufacturerTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.supplierTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.priceNumeric);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.unitTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.articulTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "AddOrEditForm";
             this.Text = "AddOrEditForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.priceNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.discountNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox articulTextBox;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox unitTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown priceNumeric;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox supplierTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox manufacturerTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown discountNumeric;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown countNumeric;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox descriptionTextBox;
+        private System.Windows.Forms.Button addPicButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
