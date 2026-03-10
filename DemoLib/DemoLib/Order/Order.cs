2377a17bde05ab0c1a24beb4d5f775cc85549f21
sellasouls666 <pgbc303@gmail.com>
2026-03-10 14:54:17 +0300

    сделал модель заказов

---
+++ DemoLib/DemoLib/Order/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib.Order
+{
+    public class Order
+    {
+        public int id_ { get; set; }
+        public DateTime orderDate_ { get; set; }
+        public DateTime delieveryDate_ { get; set; }
+        public int idPickup_ { get; set; }
+        public string fio_ { get; set; }
+        public int code_ { get; set; }
+        public string status_ { get; set; }
+        public string userLogin_ { get; set; }
+        public List<OrderProductRecord> orderProducts { get; set; }
+    }
+}
---
+++ DemoLib/DemoLib/Order/OrderProductRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib.Order
+{
+    public class OrderProductRecord
+    {
+        public int id_ { get; set; }
+        public string articul_ { get; set; }
+        public int quantity_ { get; set; }
+    }
+}
