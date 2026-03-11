d4e51746bb23c162354ec2205444a60fbb072461
sellasouls666 <pgbc303@gmail.com>
2026-03-11 14:50:51 +0300

    сделал внешний вид формы добавления и редактирования заказов

---
+++ DemoForm/AddOrEditOrderForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace DemoForm
+{
+    partial class AddOrEditOrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idLabel = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addressComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.orderDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateDelieveryTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.cancelOrderButton = new System.Windows.Forms.Button();
+            this.okOrderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.idLabel.Location = new System.Drawing.Point(21, 18);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(28, 19);
+            this.idLabel.TabIndex = 0;
+            this.idLabel.Text = "ID:";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Enabled = false;
+            this.idTextBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.idTextBox.Location = new System.Drawing.Point(149, 15);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(130, 26);
+            this.idTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Статус:";
+            // 
+            // statusComboBox
+            // 
+            this.statusComboBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statusComboBox.FormattingEnabled = true;
+            this.statusComboBox.Items.AddRange(new object[] {
+            "Новый",
+            "Завершен"});
+            this.statusComboBox.Location = new System.Drawing.Point(149, 60);
+            this.statusComboBox.Name = "statusComboBox";
+            this.statusComboBox.Size = new System.Drawing.Size(130, 27);
+            this.statusComboBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(21, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Адрес ПВЗ:";
+            // 
+            // addressComboBox
+            // 
+            this.addressComboBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addressComboBox.FormattingEnabled = true;
+            this.addressComboBox.Location = new System.Drawing.Point(149, 105);
+            this.addressComboBox.Name = "addressComboBox";
+            this.addressComboBox.Size = new System.Drawing.Size(130, 27);
+            this.addressComboBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(21, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Дата заказа:";
+            // 
+            // orderDateTimePicker
+            // 
+            this.orderDateTimePicker.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.orderDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.orderDateTimePicker.Location = new System.Drawing.Point(149, 155);
+            this.orderDateTimePicker.Name = "orderDateTimePicker";
+            this.orderDateTimePicker.Size = new System.Drawing.Size(130, 26);
+            this.orderDateTimePicker.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(21, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Дата выдачи:";
+            // 
+            // dateDelieveryTimePicker
+            // 
+            this.dateDelieveryTimePicker.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateDelieveryTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateDelieveryTimePicker.Location = new System.Drawing.Point(149, 199);
+            this.dateDelieveryTimePicker.Name = "dateDelieveryTimePicker";
+            this.dateDelieveryTimePicker.Size = new System.Drawing.Size(130, 26);
+            this.dateDelieveryTimePicker.TabIndex = 9;
+            // 
+            // cancelOrderButton
+            // 
+            this.cancelOrderButton.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.cancelOrderButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelOrderButton.Location = new System.Drawing.Point(44, 256);
+            this.cancelOrderButton.Name = "cancelOrderButton";
+            this.cancelOrderButton.Size = new System.Drawing.Size(75, 36);
+            this.cancelOrderButton.TabIndex = 10;
+            this.cancelOrderButton.Text = "Отмена";
+            this.cancelOrderButton.UseVisualStyleBackColor = false;
+            // 
+            // okOrderButton
+            // 
+            this.okOrderButton.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.okOrderButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.okOrderButton.Location = new System.Drawing.Point(174, 256);
+            this.okOrderButton.Name = "okOrderButton";
+            this.okOrderButton.Size = new System.Drawing.Size(75, 36);
+            this.okOrderButton.TabIndex = 11;
+            this.okOrderButton.Text = "ОК";
+            this.okOrderButton.UseVisualStyleBackColor = false;
+            // 
+            // AddOrEditOrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 324);
+            this.Controls.Add(this.okOrderButton);
+            this.Controls.Add(this.cancelOrderButton);
+            this.Controls.Add(this.dateDelieveryTimePicker);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.orderDateTimePicker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.statusComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.idLabel);
+            this.Name = "AddOrEditOrderForm";
+            this.Text = "AddOrEditOrderForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox statusComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox addressComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker orderDateTimePicker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateDelieveryTimePicker;
+        private System.Windows.Forms.Button cancelOrderButton;
+        private System.Windows.Forms.Button okOrderButton;
+    }
+}
