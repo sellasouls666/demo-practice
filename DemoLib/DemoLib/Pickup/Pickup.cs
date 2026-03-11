d2fe76e2064e752bcce640386381fab166a66f7d
sellasouls666 <pgbc303@gmail.com>
2026-03-11 15:10:00 +0300

    сделал модель пункта выдачи

---
+++ DemoLib/DemoLib/Pickup/Pickup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLib.Pickup
+{
+    public class Pickup
+    {
+        public string address_ { get; set; }
+        public int id_ { get; set; }
+    }
+}
